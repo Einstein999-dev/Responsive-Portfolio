e265bb6a23354bd009ec7bdbecacdf87a6381a96
Dhxuv-span <71958287+Einstein999-dev@users.noreply.github.com>
2022-11-08 20:08:19 +0530

    New

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// $(function() {
+//     $("a.page-scroll").bind("click", function(e) {
+//         var t = $(this);
+//         $("html, body").stop().animate({
+//             scrollTop: (parseInt($(t.attr("href")).offset().top)-40)
+//         }, 1500, "easeInOutExpo");
+//         e.preventDefault()
+//     })
+// });
+
+// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#navId'
+// })
